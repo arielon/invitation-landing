d7f103c8db96731dfd7337b50234adf1df30dbfe
arielon <43272158+arielon@users.noreply.github.com>
2025-01-02 16:38:23 -0600

    vite actualizado

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		port: process.env.PORT || 3000, // Usar el puerto asignado o 3000 por defecto
-		host: true, // Permite la exposición de la aplicación
-	},
-	preview: {
-		port: process.env.PORT || 3000, // Usar el puerto asignado o 3000 por defecto
-		host: true, // Permite la exposición de la aplicación
-	},
-	base: process.env.NODE_ENV === "production" ? "/" : "/", // Ajusta la base para producción si es necesario
 });
