211c3dc7a65836a66dd34d51b63694485dc6972f
arielon <43272158+arielon@users.noreply.github.com>
2025-01-02 17:09:44 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/", // Asegúrate de que este sea el valor correcto para tu aplicación
+	server: {
+		port: 3000, // Cambia según sea necesario
+	},
+	build: {
+		outDir: "dist", // Asegúrate de que Render pueda encontrar esta carpeta
+	},
 });
